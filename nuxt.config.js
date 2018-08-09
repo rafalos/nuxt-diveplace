3fee48f6a3b123371d9131b57b8899c704e9ad3f
rafalos <rsulkowski1@gmail.com>
2018-08-09 22:02:46 +0200

    fix for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 module.exports = {
   mode: 'universal',
   env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+    baseUrl: process.env.IP || 'http://localhost:3000'
   },
   /*
   ** Headers of the page
